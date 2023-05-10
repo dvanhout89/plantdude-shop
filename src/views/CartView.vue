9a9a52e61db252cf7e40cb7d7111fc26b4ac8f7f
Lerato Malinga <lmalinga@retrorabbit.co.za>
2023-05-10 15:13:46 +0200

    Card update

--- src/views/CartView.vue
+++ src/views/CartView.vue
@@ -3,47 +3,42 @@
         <h1 class="view-title">Cart</h1>
 
         <div class="container product-card">
-            <div class="card px-2">
+    <div class="card px-2">
+        <div class="row">
+            <div class="col-4 col-md-2 " id="img-col">
+                <img src="../assets/mushroom.jpg" class="img" alt="Picture of Mushroom Related Medicine">
+            </div>
+            <div class="col-4 col-md-4">
+                <div class="row product-details">
+                    <div class="col-12" id="product-name">
+                        <div class="card-title">REISHI, CHAGA & ECHINACEA TINCTURE</div>
+                        <hr>
+                    </div>
+                    <div class="col-12">
+                        <div class="label" id="product-category">Category Name</div>
+                        <div class="label" id="product-size">100ml</div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-4 col-md-4">
                 <div class="row">
                     <div class="col-12">
-                        <div class="row">
-                            <div class="col-4 col-md-2 offset-md-2" id="img-col">
-                                <img src="../assets/mushroom.jpg" class="img" alt="Picture of Mushroom Related Medicine">
-                            </div>
-                            <div class="col-4 col-md-4">
-                                <div class="row product-details">
-                                    <div class="col-12" id="product-name">
-                                        <div class="card-title">REISHI, CHAGA & ECHINACEA TINCTURE</div>
-                                        <hr>
-                                    </div>
-                                    <div class="col-12">
-                                       
-                                        <div class="label" id="product-category">Category Name</div>
-                                        <div class="label" id="product-size">100ml</div>
-                                    </div>
-                                </div>
-                            </div>
-                            <div class="col-4 col-md-2">
-                                <div class="row">
-                                    <div class="col-12">
-                                        <div>
-                                            <div class="label" id="product-price">R 130.00</div>
-                                        </div>
-                                        <div class="form-outline">
-                                            <input type="number" id="typeNumber" class="form-control" />
-                                        </div>
-                                    </div>
-                                    <div class="col-12">
-                                        <div class="btn" id="add-btn">Remove</div>
-                                    </div>
-
-                                </div>
-                            </div>
+                        <div>
+                            <div class="label" id="product-price">R 130.00</div>
                         </div>
+                        <div class="form-outline">
+                            <input type="number" id="typeNumber" class="form-control" />
+                        </div>
+                    </div>
+                    <div class="col-12">
+                        <div class="btn" id="add-btn">Remove</div>
                     </div>
                 </div>
             </div>
         </div>
+    </div>
+</div>
+
         <div class="subtotal">
             <div class="mb-4">
                 <h4>Subtotal</h4>
@@ -67,6 +62,7 @@
 
 .card {
     bottom: 10px;
+    margin-top: 40px;
 }
 
 #img-col {
@@ -75,15 +71,17 @@
     justify-content: center;
     align-items: center;
 }
-#product-price{
-    margin-top: 10px;
-
-}
 
 .img {
     width: 120%;
     border-radius: 50%;
 }
+#product-price{
+    margin-top: 10px;
+
+}
+
+
 .form-outline{
     margin-top: 15px;
   
@@ -118,6 +116,7 @@
         justify-content: center;
         align-items: center;
         height: 20vh;
+        width: fit-content;
     }
 
     #add-btn {
@@ -167,7 +166,7 @@ p {
 }
 
 .img {
-    width: 100%;
+    width: 130px;
     border-radius: 50%;
     height: 20vh;
 }
